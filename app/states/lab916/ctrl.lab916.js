c2259771e1d337f8d06b557962067652bd3b3092
Julius Alvarado <javascript.uiux@gmail.com>
2018-01-09 15:51:36 -0800

    cleaned up some more stuff

--- app/states/lab916/ctrl.lab916.js
+++ app/states/lab916/ctrl.lab916.js
@@ -19,7 +19,7 @@
             vm.heroTitle = "SKIP THE LEARNING CURVE OF SELLING YOUR BRAND ON AMAZON.";
             vm.heroPara = "Lab 916 a creative marketing agency in Sacramento, CA, that helps Amazon sellers get more sales. We take the guesswork out of selling on Amazon by crafting compelling, persuasive listings, improving PPC performance, and driving sales. In short: you have the amazing products, and we have the skills and expertise to get them into customer’s hands.";
             vm.heroQuote = "LAB916 isn’t your typical amateur Amazon marketing agency with zero experience. They actually increase traffic to your listing and more importantly, the vitalness of conversion. If you’re serious about growing your FBA business, give 10 minutes of your time to LAB916.";
-            vm.heroImg = "./l9-material/app/images/ill/hero-img.png";
+            vm.heroImg = "images/ill/hero-img.png";
 
             vm.moSalesPara = "We simplify your work at all stages. Your customers will enjoy participating in the sales strategy process, and they will see the beautiful and understandable structure of the amazon listing";
 
@@ -42,7 +42,11 @@
 
             vm.advertisingPara = "We will also take care of advertising, creating sponsored product ads and using Amazon Marketing Services (AMS) to drive more clicks to your products and bring conversions that turn into sales. These services can be wieldy, but we have the expertise to navigate them deftly and keep your ACoS% low. ";
             vm.contentOptPara = "Content optimization involves the branding and presentation of your Amazon listings. It’s easy to get lost in a sea of products in the ever-expanding Amazon universe— we know how to make yours stand out. Well-crafted design and copy go a long way in catching the attention of a customer, and we are experts in producing compelling examples of both. In addition, extensive keyword research has given us a deep understanding of the nuances of Amazon rankings, and we can bring yours to the top. ";
+
+            vm.channelManage = "Channel Management";
             vm.channelManagePara = "We don’t wait for you to tell us what you want for your channel, we take proactive steps to leverage Amazon’s technologies on your behalf. Avoid the beginner mistakes that can cost you time and money, and allow our expertise to get your listings set up just the way Amazon likes them. We’ll help you with listing management, repricer setup, brand registration, and international selling to make sure that you get the most out of your Amazon presence.  ";
+
+
         }
     ]);
 }());
