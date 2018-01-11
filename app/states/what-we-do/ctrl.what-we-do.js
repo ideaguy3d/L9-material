1a87a54022f32dbbb74203e1bcca28ef5043432d
Julius Alvarado <javascript.uiux@gmail.com>
2018-01-11 15:21:08 -0800

    got some of the content-optimization ngView coded

--- app/states/what-we-do/ctrl.what-we-do.js
+++ app/states/what-we-do/ctrl.what-we-do.js
@@ -2,7 +2,6 @@
     angular.module('L9-app').controller('WhatWeDoCtrl', ['$rootScope', 'homeSubFooterState',
         function ($rootScope, homeSubFooterState) {
             var vm = this;
-            console.log("jha - homeSubFooterState = "+homeSubFooterState.curState);
             vm.title = "(: What We Do! ^_^ :)";
             $rootScope.ccShowHomeSubFooter = homeSubFooterState.mode;
         }
