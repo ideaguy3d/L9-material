5fc54efb4f8d690ef67d48b0718e44fb8348b76f
Julius Alvarado <javascript.uiux@gmail.com>
2018-01-25 12:10:30 -0800

    REWRITING landing page UI, it's going to be pretty different

--- app/states/lab916/ctrl.lab916.js
+++ app/states/lab916/ctrl.lab916.js
@@ -8,12 +8,11 @@
     angular.module('L9-app').controller('Lab916Ctrl', ["$rootScope", "homeSubFooterState", "smoothScroll",
         function ($rootScope, homeSubFooterState, smoothScroll) {
             const vm = this;
-            console.log("jha - Lab916Ctrl invoked ^_^");
 
             $rootScope.ccShowHomeSubFooter = homeSubFooterState.mode;
 
-            vm.slogan = "We help amazon sellers get more sales.";
-            vm.slogan2 = "AMAZON OPTIMIZATION SPECIALISTS";
+            vm.slogan = "We help Amazon sellers get more sales.";
+            vm.slogan2 = "Amazon Optimization Specialists";
 
             vm.iconAd = "http://lab916.wpengine.com/mock2/images/icon-ad.png";
             vm.iconCc = "http://lab916.wpengine.com/mock2/images/icon-cc.png";
@@ -42,13 +41,14 @@
             vm.kleinQuote = "It’s rare to find Amazon marketers who understand ROI and actually act on it. LAB916 knows PPC, they know design, and more importantly, they execute. They don’t care about worthless vanity metrics, they care about making you more money.";
             vm.sellerCentralImg1 = "http://lab916.wpengine.com/mock2/images/hero-channel-management.png";
 
-            vm.whatWeDo = "We work hard to make sure your products get the sales that they deserve on the Amazon marketplace. Our services include Content Optimization, Advertising, and Channel Management: three key elements to make your product rise above the competition.";
+            vm.whatWeDo = "We work hard to make sure your products get the sales that they deserve. Our services include:";
 
             vm.advertisingPara = "We will also take care of advertising, creating sponsored product ads and using Amazon Marketing Services (AMS) to drive more clicks to your products and bring conversions that turn into sales. These services can be wieldy, but we have the expertise to navigate them deftly and keep your ACoS% low. ";
             vm.contentOptPara = "Content optimization involves the branding and presentation of your Amazon listings. It’s easy to get lost in a sea of products in the ever-expanding Amazon universe— we know how to make yours stand out. Well-crafted design and copy go a long way in catching the attention of a customer, and we are experts in producing compelling examples of both. In addition, extensive keyword research has given us a deep understanding of the nuances of Amazon rankings, and we can bring yours to the top. ";
 
             vm.channelManage = "Channel Management";
-            vm.channelManagePara = "We don’t wait for you to tell us what you want for your channel, we take proactive steps to leverage Amazon’s technologies on your behalf. Avoid the beginner mistakes that can cost you time and money, and allow our expertise to get your listings set up just the way Amazon likes them. We’ll help you with listing management, repricer setup, brand registration, and international selling to make sure that you get the most out of your Amazon presence.  ";
+            vm.channelManagePara = "We don’t wait for you to tell us what you want for your channel, we take proactive steps.";
+
         }
     ]);
 }());
